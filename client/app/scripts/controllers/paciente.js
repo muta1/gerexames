7fdae8cef45306698a7ef821cd94487f2b3a8f31
muta1 <italo1577@gmail.com>
2018-06-02 15:32:52 -0300

    Paciente
    Continuing Paciente page

--- client/app/scripts/controllers/paciente.js
+++ client/app/scripts/controllers/paciente.js
@@ -8,7 +8,7 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('PacienteCtrl', ['$scope', '$http', 'API', 'pacienteServices', 'NgTableParams', '$mdSidenav', '$location', 'microAreaServices', function ($scope, $http, API, pacienteServices, NgTableParams, $mdSidenav, $location, microAreaServices) {
+  .controller('PacienteCtrl', ['$scope', '$http', 'API', 'pacienteServices', 'NgTableParams', '$mdSidenav', '$location', 'microAreaServices', '$filter', function ($scope, $http, API, pacienteServices, NgTableParams, $mdSidenav, $location, microAreaServices, $filter) {
     // -------------------------------------------
     //$scope.listPacientesMicroarea = {};
     //$scope.listMicroAreas = {};
@@ -43,6 +43,7 @@ angular.module('clientApp')
         $scope.microAreaBuscada = mAreaObj;
         //console.log('oi meu chapa ',response.data.rows )
         $scope.listPacientesMicroarea = response.data.rows;
+        console.log('Todos pacientes da microarea - ', $scope.listPacientesMicroarea);
         if ($scope.listPacientesMicroarea.length > 0) {
           $scope.listPacientesFoiEncontrado = true;
           $scope.listPacientesNenhumEncontrado = false;
@@ -58,9 +59,134 @@ angular.module('clientApp')
     }
 
     // -------------------------------------------
-    // ADICIONAR PACIENTE
-    $scope.addPaciente = false;
-    $scope.adicionarPaciente = function (){
-      $scope.addPaciente = true;
+    // INCOMUM ENTRE PACIENTES
+    $scope.dataPaciente = {};
+    $scope.formPaciente = false;
+    $scope.clearPaciente = function () {
+      $scope.dataPaciente = {};
     }
+
+    // -------------------------------------------
+    // ADICIONAR PACIENTE
+
+    $scope.addPaciente = false;
+    $scope.adicionarPaciente = function () {
+      $scope.formPaciente = true;
+      $scope.clearPaciente();
+      $scope.addPaciente = true;
+      $scope.editPaciente = false;
+    }
+
+    // -------------------------------------------
+    // Editar Paciente
+    $scope.editPaciente = false;
+    $scope.editarPaciente = function (paciente) {
+      $scope.formPaciente = true;
+
+      $scope.editPaciente = true;
+      $scope.addPaciente = false;
+      console.log('paciente ao clicar em editar paciente - ', paciente)
+      // novo Objeto Paciente
+      $scope.dataPaciente.id = paciente.id;
+      $scope.dataPaciente.EM_PRIORIDADE = 1;
+      $scope.dataPaciente.NOME_PACIENTE = paciente.NOME_PACIENTE;
+      $scope.dataPaciente.DATA_NASC = new Date(paciente.DATA_NASC);
+      $scope.dataPaciente.TELEFONE_FIXO = paciente.TELEFONE_FIXO;
+      $scope.dataPaciente.TELEFONE_CELULAR = paciente.TELEFONE_CELULAR;
+      $scope.dataPaciente.RUA = paciente.RUA;
+      $scope.dataPaciente.NUMERO = paciente.NUMERO;
+      $scope.dataPaciente.BAIRRO = paciente.BAIRRO;
+      $scope.dataPaciente.CEP = paciente.CEP;
+      $scope.dataPaciente.EXAME_MASTOLOGIA_ALTERADO = paciente.EXAME_MASTOLOGIA_ALTERADO.toString();
+      $scope.dataPaciente.EXAME_MASTOLOGIA_DESC = paciente.EXAME_MASTOLOGIA_DESC;
+      $scope.dataPaciente.EXAME_CITOPATOLOGICO_ALTERADO = paciente.EXAME_CITOPATOLOGICO_ALTERADO.toString();
+      $scope.dataPaciente.EXAME_CITOPATOLOGICO_DESC = paciente.EXAME_CITOPATOLOGICO_DESC;
+
+      if ($scope.dataPaciente.EXAME_MASTOLOGIA_ALTERADO == '1' || $scope.dataPaciente.EXAME_CITOPATOLOGICO_ALTERADO == '1') {
+        $scope.dataPaciente.EM_PRIORIDADE = 1;
+      } else {
+        $scope.dataPaciente.EM_PRIORIDADE = 0;
+      }
+
+
+
+      //$scope.dataPaciente.NOME_PACIENTE = paciente.NOME_PACIENTE;
+
+      //$scope.dataPaciente.DATA_NASC = //new Date(paciente.DATA_NASC).toLocaleDateString();
+    }
+
+    // -------------------------------------------
+    // Remover Paciente
+
+
+
+
+
+    // --------------------------------------------
+    // ADD PACIENTE
+    $scope.adicionarPacienteFormulario = function (paciente) {
+      console.log(paciente);
+      pacienteServices.putPaciente(paciente);
+
+    }
+
+    // --------------------------------------------
+    // ATUALIZAR PACIENTE
+    $scope.atualizarPacienteFormulario = function (paciente) {
+      console.log(paciente);
+      //if()
+
+      // var pacienteData = paciente; //console.log('paciente :', paciente);
+      // var _atualizarPaciente = pacienteServices.putPaciente(pacienteData);
+
+      // _atualizarPaciente.then(function (res) {
+      //   console.log('atualizou - ', res.data);
+      // }, function (err) {
+      //   console.log('deu erro - ', err);
+      // })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    // --------------------------------------------
+    // CONTROLE DE EXAMES ALTERADO OU NÃO
+    // CITOPATOLOGICO
+    $scope.varAlteradoCito = false;
+    $scope.alteradoCito = function () {
+      $scope.varAlteradoCito = true;
+    }
+    $scope.naoAlteradoCito = function () {
+      $scope.varAlteradoCito = false;
+      $scope.dataPaciente.EXAME_CITOPATOLOGICO_DESC = "";
+    }
+
+    // MASTOLOGIA
+    $scope.varAlteradoMasto = false;
+    $scope.alteradoMasto = function () {
+      $scope.varAlteradoMasto = true;
+    }
+    $scope.naoAlteradoMasto = function () {
+      $scope.varAlteradoMasto = false;
+      $scope.dataPaciente.EXAME_MASTOLOGIA_DESC = "";
+    }
+
+
+
+    $scope.submitForm = function (valid) {
+      if (valid) {
+        console.log('form-paciente válido', valid)
+      } else {
+        console.log('form-paciente inválido', valid)
+      }
+    }
+
   }]);
--- client/app/scripts/services/paciente-service.js
+++ client/app/scripts/services/paciente-service.js
@@ -14,17 +14,71 @@ angular.module('clientApp')
 
       },
       delTodosPacientesMicroarea: function (microareaID) {
-        var data = {
+        let data = {
           meusDados: microareaID
         }
         return $http.post(API + '/paciente/deleteFromMicroarea/', data);
       },
-      getPacientesFromMicroarea: function(microareaID){
-        var data = {
+      getPacientesFromMicroarea: function (microareaID) {
+        let data = {
           meusDados: microareaID
         }
         return $http.post(API + '/paciente/pacientesFromMicroarea/', data);
+      },
+      putPaciente: function (paciente) {
+
+
+        let data = {
+          BAIRRO: paciente.BAIRRO,
+          //     //   ​
+          CEP: paciente.CEP,
+          //     //   ​
+          DATA_NASC: paciente.DATA_NASC.toString(),
+          //     //   ​
+          EM_PRIORIDADE: paciente.EM_PRIORIDADE,
+          //     //   ​
+          //     //   EXAME_CITOPATOLOGICO_ALTERADO: parseInt(paciente.EXAME_CITOPATOLOGICO_ALTERADO),
+          //     //   ​
+          //     //   EXAME_CITOPATOLOGICO_DESC: paciente.EXAME_CITOPATOLOGICO_DESC,
+          //     //   ​
+          //     //   EXAME_MASTOLOGIA_ALTERADO: parseInt(paciente.EXAME_MASTOLOGIA_ALTERADO),
+          //     //   ​
+          //     //   EXAME_MASTOLOGIA_DESC: paciente.EXAME_MASTOLOGIA_DESC,
+          //     //   ​
+          //     //   NOME_PACIENTE: paciente.NOME_PACIENTE,
+          //     //   ​
+          //     //   NUMERO: paciente.NUMERO,
+          //     //   ​
+          //     //   RUA: paciente.RUA,
+          //     //   ​
+          //     //   TELEFONE_CELULAR: paciente.TELEFONE_CELULAR,
+          //     //   ​
+          //     //   TELEFONE_FIXO: paciente.TELEFONE_FIXO
+        }
+        //console.log('data put -> ', data);
+        console.log('paciente:', data);
+        return $http.patch(API + '/paciente/'+paciente.id,data);
       }
+      // putPaciente:function(paciente){
+      //   let data = {
+      //     BAIRRO: paciente.BAIRRO,
+      //     CEP: paciente.CEP,
+      //     CODIGO_MICROAREA: paciente.CODIGO_MICROAREA,​​
+      //     DATA_NASC: paciente.DATA_NASC.toString(),          ​​
+      //     EM_PRIORIDADE: paciente.EM_PRIORIDADE,          ​​
+      //     EXAME_CITOPATOLOGICO_ALTERADO: paciente.EXAME_CITOPATOLOGICO_ALTERADO,
+      //     EXAME_CITOPATOLOGICO_DESC: paciente.EXAME_CITOPATOLOGICO_DESC,
+      //     EXAME_MASTOLOGIA_ALTERADO: paciente.EXAME_MASTOLOGIA_ALTERADO,
+      //     EXAME_MASTOLOGIA_DESC: paciente.EXAME_MASTOLOGIA_DESC,
+      //     NOME_PACIENTE: paciente.NOME_PACIENTE,
+      //     NUMERO: paciente.NUMERO,​​
+      //     RUA: paciente.RUA,​​
+      //     TELEFONE_CELULAR: paciente.TELEFONE_CELULAR,          ​​
+      //     TELEFONE_FIXO: paciente.TELEFONE_FIXO
+      //   }
+      //   console.log('data put -> '+ data);
+      //   //return $http.put(API + '/paciente/'+paciente.id,data);
+      // }
     };
 
   }]);
